71795869e7bc71023e361d1dd8ac4986deecf661
Wings Entregas <wingsentregas@gmail.com>
2021-03-05 12:04:04 -0300

    First Commit of project imc
    First commit of the imc calculator project

---
+++ calculadora_imc/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:calculadora_imc/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Calculadora basico de IMC',
+    home: HomePage(),
+  ));
+}
